181dcfd858307b48a2a7798337a96e25daffa274
michael <michael@peoplelikesoftware.com>
2024-11-09 14:45:40 -0500

    add env file and set the val

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,35 @@ use std::io;
 use std::thread;
 use std::time::Duration;
 use dotenv;
-use std::process;
+use std::env;
+use std::fs;
+use std::path::Path;
+
+const BAT_MIN: f32 = 5.0;
+const BAT_MAX: f32 = 50.0;
+const BATTERY_MIN_THRESHOLD: &str = "BATTERY_MIN_THRESHOLD";
 
 fn main() -> battery::Result<()> {
-    dotenv::dotenv().ok();
-    let battery_threshold = dotenv::var("BATTERY_THRESHOLD")
-        .and_then(|v| {
-            v.parse::<f32>().map_err(|e| {
-                eprintln!("Error converting BATTERY_THRESHOLD to f32: {:#?}", e);
-                eprintln!("Please ensure the BATTERY_THRESHOLD env variable is a valid float.");
-                process::exit(1);
-            })
-        })
-        .unwrap_or_else(|e|{
-            eprintln!("Error retrieving env BATTERY_THRESHOLD: {:#?}", e);
-            process::exit(1);
-        });
 
     let manager = Manager::new()?;
+
+    let battery_threshold: f32 = match get_battery_threshold() {
+        Ok(val) => {
+            println!("{}", val);
+            val
+        }
+        Err(_) => {
+            loop {
+                match set_environment_var() {
+                    Ok(val) => {
+                        break val;
+                    }
+                    Err(e) => println!("{:#?}", e)
+                }
+            }
+        }
+    };
+
     let mut battery = match manager.batteries()?.next() {
         Some(Ok(battery)) => battery,
         Some(Err(e)) => {
@@ -43,3 +54,49 @@ fn main() -> battery::Result<()> {
         manager.refresh(&mut battery)?;
     }
 }
+
+
+fn set_environment_var() -> Result<f32, String> {
+
+
+    println!("Please enter a your battery limit (Must be between 5 and 50 inclusive)");
+    let mut input = String::new();
+
+    if let Err(e) = std::io::stdin().read_line(&mut input) {
+        return Err(format!("Failed to read input: {}", e));
+    }
+
+    let input: f32 = input.trim().parse().map_err(|e| {
+        format!("Invalid input. Please enter a valid integer. Error: {}", e)
+    })?;
+
+    if input > BAT_MAX || input  < BAT_MIN {
+        return Err(String::from("Error: 'BATTERY_MIN_THESHOLD' must be greater than 4 but less than 50"));
+    } 
+
+    env::set_var(BATTERY_MIN_THRESHOLD, input.to_string());
+
+    if !Path::new(".env").exists() {
+        if let Err(e) = fs::write(".env", format!("{BATTERY_MIN_THRESHOLD}={}", input.to_string())) {
+            return Err(e.to_string());
+        }
+    }
+
+    Ok(input)
+}
+
+fn get_battery_threshold() -> Result<f32, String>{
+
+    dotenv::dotenv().ok();
+    let battery_threshold = dotenv::var("BATTERY_MIN_THRESHOLD")
+        .map_err(|_| String::from("Error: Environment variable 'BATTERY_MIN_THRESHOLD' is not set"))?;
+
+    let battery_threshold: f32 = battery_threshold
+        .parse()
+        .map_err(|_| String::from("Error: 'BATTERY_MIN_THRESHOLD' is not a valid float."))?;
+    if battery_threshold > BAT_MAX || battery_threshold < BAT_MIN {
+        return Err(String::from("Error: 'BATTERY_MIN_THESHOLD' must be greater than 4 but less than 50"));
+    } 
+    Ok(battery_threshold)
+}
+
